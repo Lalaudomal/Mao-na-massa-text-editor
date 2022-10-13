a5f9cd24dc0b4a083a04c325c7d0fcfe3064e748
Lalaudomal <115725044+Lalaudomal@users.noreply.github.com>
2022-10-13 15:45:44 -0300

    Add files via upload

---
+++ Texteditor/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Menu();
+    }
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("o que voce deseja fazer? ");
+        Console.WriteLine("1 - abrir arquivo");
+        Console.WriteLine("2 - criar novo arquivo");
+        Console.WriteLine("0 - sair");
+        short option = short.Parse(Console.ReadLine());
+
+        switch (option)
+        {
+            case 0: System.Environment.Exit(0); break;
+            case 1: Abrir(); break;
+            case 2: Editar(); break;
+            default: Menu(); break;
+        }
+    }
+    static void Abrir()
+    {
+        Console.Clear();
+        Console.WriteLine("qual caminho do arquivo?");
+        string path = Console.ReadLine();
+
+        using (var file = new StreamReader(path))
+        {
+            string text = file.ReadToEnd();
+            Console.WriteLine(text);
+        }
+        Console.WriteLine("");
+        Console.ReadLine();
+        Menu();
+    }
+    static void Editar()
+    {
+        Console.Clear();
+        Console.WriteLine("digite seu texto abaixo(esc para sair");
+        Console.WriteLine("---------");
+        string text = "";
+
+        do
+        {
+            text += Console.ReadLine();
+            text += Environment.NewLine;
+        }
+        while (Console.ReadKey().Key != ConsoleKey.Escape);
+        Salvar(text);
+    }
+    static void Salvar(string text)
+    {
+        Console.Clear();
+        Console.WriteLine("local para salvar o arquivo?");
+        var path = Console.ReadLine();
+
+        using (var file = new StreamWriter(path))
+        {
+            file.Write(text);
+        }
+        Console.WriteLine($"arquivo {path} salvo com sucesso");
+        Console.ReadLine();
+        Menu();
+    }
+}
